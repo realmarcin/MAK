c2449fa176ec4a77e2d80d208c968c3109c11a6b
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-03-04 10:29:32 -0800

    wrong path

--- MAK_build/antbuild_git.sh
+++ MAK_build/antbuild_git.sh
@@ -2,7 +2,7 @@
 #source ~/build_java.cmd
 #cvs update &> antbuild.out
 #-Dfile.encoding=UTF-8
-cd /usr2/people/marcin/Genomics/html/applet/MicroArrayUp/MicroArrayUp/MAK_build_git/MAK
+#cd /usr2/people/marcin/Genomics/html/applet/MicroArrayUp/MicroArrayUp/MAK_build_git/MAK
 git pull
 cd ..
 ant -verbose -buildfile MAKantbuild_git.xml &> antbuild_git.out
