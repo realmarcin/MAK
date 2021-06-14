2e8fa039c480b08b1982d6e8e75c484e5863f3ce
realmarcin <4625870+realmarcin@users.noreply.github.com>
2021-06-14 13:04:22 -0700

    RLE always wants signed data

--- DataMining/MAKflow_JBEI_SLURM_v2.java
+++ DataMining/MAKflow_JBEI_SLURM_v2.java
@@ -626,8 +626,8 @@ public class MAKflow_JBEI_SLURM_v2 {
             } else if (start_method.equalsIgnoreCase("RLE")) {
                 int rle_useAbs =0;//useAbs;
 
-                Rstarts_script += "nbs1=allpossibleInitialRLE(expr_data_row,useAbs=" + useAbs + ", isCol=1,discretize_step="+rlestep+",min_run_length="+rlemin+")\n";
-                Rstarts_script += "nbs2=allpossibleInitialRLE(expr_data_col,useAbs=" + useAbs + ", isCol=0,discretize_step="+rlestep+",min_run_length="+rlemin+")\n";
+                Rstarts_script += "nbs1=allpossibleInitialRLE(expr_data_row,useAbs=" + rle_useAbs + ", isCol=1,discretize_step="+rlestep+",min_run_length="+rlemin+")\n";
+                Rstarts_script += "nbs2=allpossibleInitialRLE(expr_data_col,useAbs=" + rle_useAbs + ", isCol=0,discretize_step="+rlestep+",min_run_length="+rlemin+")\n";
                 Rstarts_script += "nbsall <- c(nbs1, nbs2)\n";
             }
 
