c3b66ab07189e9ee64a7552e2165885f256bc9e1
realmarcin <4625870+realmarcin@users.noreply.github.com>
2022-04-06 12:16:29 -0700

    --vanilla

--- DataMining/InitRandVar.java
+++ DataMining/InitRandVar.java
@@ -223,7 +223,7 @@ public class InitRandVar {
      */
     public void initR() {
         //vanilla = –no-save, –no-environ, –no-site-file, –no-init-file and –no-restore
-        String[] R_args = {"–-vanilla"};
+        String[] R_args = {"--vanilla"};
         System.out.println("RunMinerBack standard: starting Rengine");
         Rengine = new Rengine(R_args, false, new TextConsole());
         Rengine.DEBUG = 0;
