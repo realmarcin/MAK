c8e3cbb3d1d5e1b8e999c568c749d92c4718ce17
realmarcin <4625870+realmarcin@users.noreply.github.com>
2022-09-03 17:47:09 -0700

    compatible with newer JRI

--- MAK_build/antbuild_git.sh
+++ MAK_build/antbuild_git.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-#source ~/build_java.cmd
-#cvs update &> antbuild.out
-#-Dfile.encoding=UTF-8
-#cd /usr2/people/marcin/Genomics/html/applet/MicroArrayUp/MicroArrayUp/MAK_build_git/MAK
 git pull
-#cd ..
 ant -verbose -buildfile MAKantbuild_git.xml &> antbuild_git.out
